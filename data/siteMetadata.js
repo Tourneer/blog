a20c3849d35f93488f91ee7229892fcf72947c10
Sasa Fister <sashafishter@gmail.com>
2022-12-22 16:10:28 +0100

    Change title

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Tourneer blog',
+  author: 'Saša Fišter',
+  headerTitle: 'Tourneer',
+  description: `A blog about what's new in the world of tennis.`,
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
