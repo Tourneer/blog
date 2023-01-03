5b04e090fce3ee5dd45cae94e854345c77b7c760
Sasa Fister <sashafishter@gmail.com>
2023-01-03 11:56:20 +0100

    Best pracites to organize tournament

---
+++ data/authors/matija.md
@@ -0,0 +1,14 @@
+---
+name: Matija Zlatarek
+avatar: /static/images/matija.jpg
+occupation: Performance and Growth Marketing | Business Development | Blockchain | Web 3.0 | Crypto | DeFi | DAO
+company: Tourneer
+email: matija@tourneer.info
+twitter: https://twitter.com/cupofwaves
+linkedin: https://www.linkedin.com/in/matijazlatarek/
+github: https://github.com
+---
+
+Digital marketing professional with ten years of experience gained in one of the leading cybersecurity companies, several agencies, two startups, and managing and growing my own business.
+
+I have a master's degree in information technology, and my latest certification is for Decentralized Finance: The Future of Finance (Duke University Coursera four courses specialization). I have been active in the blockchain and cryptocurrency space since 2016.
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,19 @@
 const siteMetadata = {
   title: 'Tourneer blog',
-  author: 'Saša Fišter',
+  author: 'Admin',
   headerTitle: 'Tourneer',
   description: `A blog about what's new in the world of tennis.`,
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteLogo: '/static/images/only_logo.jpg',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'info@tourneer.info',
+  twitter: 'https://twitter.com/TourneerInfo',
+  facebook: 'https://www.facebook.com/tourneer',
+  linkedin: 'https://www.linkedin.com/company/tourneer/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
@@ -24,20 +22,20 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: process.env.GOOGLE_ANALYTICS, // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: '',
   },
   comment: {
     // If you want to use a commenting system other than giscus you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: '', // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
